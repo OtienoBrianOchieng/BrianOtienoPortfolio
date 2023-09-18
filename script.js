8804bc83dfbe6672e6f1dc4dead39455f1757f84
Brian Ochieng Otieno <brianochieng1989@gmail.com>
2023-09-18 08:40:09 +0300

    Reorganized the js file.

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.getElementById("submitBtn").onclick= function (){
     clientReply= document.getElementById("clientReply").value;
     clientName=document.getElementById("clientName").value;
     document.getElementById("feedback").innerHTML=clientReply;
-    document.getElementById("yourName").innerHTML= `Thank you ${clientName} for your feedback`;
-    document.getElementById("idResponded").innerHTML= `${clientName} responded`;
+    document.getElementById("yourName").innerHTML= `Thank you ${clientName} for your feedback.`;
+    document.getElementById("idResponded").innerHTML= `${clientName} responded.`;
 }
